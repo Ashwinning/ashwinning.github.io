281b29ccd7f320d2b0fcd7effb1ffc2b56f63266
Ashwin Sinha <ash@ds.gg>
2016-06-23 21:02:33 -0700

    Adding Jekyll Integration

---
+++ _site/js/skills.js
@@ -0,0 +1,46 @@
+var public_spreadsheet_url =                'https://docs.google.com/spreadsheets/d/1QXOUoGM93i4dt5XH8QbfS7TG8A00v5b-18Few3KcqfM/pubhtml';
+
+function InitializeTabletop() 
+{
+    Tabletop.init( { key: public_spreadsheet_url,
+                     callback: showInfo,
+                     simpleSheet: true } );
+}
+
+function showInfo(data, tabletop) 
+{
+    //alert("Successfully processed!")
+    console.log(data);
+    AddSkillsToPage(data);
+    //alert(data[0].Skill);
+}
+
+function CreateModule (skill, iconLink, blurb)
+{
+    	return "<div class=\"module\" id=\""+skill+"\" data-blurb=\""+blurb+"\" data-bg-img=\""+iconLink+"\" data-skill=\""+skill+"\"><div class=\"circle\" style=\"background-image:url('"+iconLink+"')\"></div><h3>"+skill+"</h3></div>"
+}
+
+function AddSkillsToPage(data)
+{
+    for(i = 0; i < data.length; i++)
+    {
+        var module = CreateModule(data[i].Skill, data[i].IconLink, data[i].Blurb);        
+        $('.autowide').append(module);
+    }
+    
+    //Add clicks to all modules
+    $('.module').click(ModuleClick);
+
+    
+}
+
+function ModuleClick(event)
+{
+        console.log('click');
+        swal({   
+            title: "<div class=\"circle\" style=\";background-image:url(\'"+$(this).attr('data-bg-img')+"\')\"></div> <h3>"+$(this).attr('data-skill')+"</h3>",   
+            text: $(this).attr('data-blurb'),   
+            html: true,
+            allowOutsideClick: true
+        });
+}
